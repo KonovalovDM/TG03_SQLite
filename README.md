834857561a1844d452f743d0e9b48a7a50d65272
KonovalovDM <kdm232006@gmail.com>
2024-12-15 17:23:43 +0300

    added code

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 
 
-1. Создайте новую базу данных school_data.db. В этой базе данных создайте таблицу students с колонками: id (INTEGER, PRIMARY KEY, AUTOINCREMENT) name (TEXT) age (INTEGER) grade (TEXT)
+1. Создайте новую базу данных school_data.db.<br>
+2. В этой базе данных создайте таблицу students с колонками:<br> id (INTEGER, PRIMARY KEY, AUTOINCREMENT) name (TEXT) age (INTEGER) grade (TEXT)
 
 
-2. Создайте Телеграм-бота, который запрашивает у пользователя имя, возраст и класс (grade), в котором он учится. Сделайте так чтоб бот сохранял введенные данные в таблицу students базы данных school_data.db.
+2. Создайте Телеграм-бота, который запрашивает у пользователя имя, возраст и класс (grade),<br> в котором он учится.<br> Сделайте так чтоб бот сохранял введенные данные в таблицу students базы данных school_data.db.
--- main.py
+++ main.py
@@ -1,25 +1,107 @@
 import asyncio
-import random
-import aiohttp
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, FSInputFile
-from googletrans import Translator
+import sqlite3
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
 from config import TOKEN
 
+# Настраиваем логирование
+logging.basicConfig(level=logging.DEBUG)
+
+# Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+storage = MemoryStorage()  # Хранилище состояний
+dp = Dispatcher(storage=storage)
 
+# Определяем состояния
+class Form(StatesGroup):
+    name = State()
+    age = State()
+    grade = State()
 
+# Инициализация базы данных
+def init_db():
+    conn = sqlite3.connect('school_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        grade TEXT NOT NULL
+    )
+    ''')
+    conn.commit()
+    conn.close()
 
+# Вызываем функцию для создания базы данных
+init_db()
+
+# Хэндлер для команды /start
 @dp.message(CommandStart)
-async def start(message: Message):
-    await message.answer(f'Приветики, {message.from_user.first_name}! Я бот! Могу показать погоду, фотки и многое другое!')
+async def start(message: Message, state: FSMContext):
+    await state.clear()  # Сбрасываем состояние
+    logging.debug("Состояние сброшено.")
+    logging.debug("Команда /start получена")
+    await message.answer('Приветики, я Школьный Бот! А тебя как зовут?')
+    await state.set_state(Form.name)
+    logging.debug("Состояние установлено: Form.name")
 
+# Хэндлер для ввода имени
+@dp.message(Form.name)
+async def get_name(message: Message, state: FSMContext):
+    logging.debug(f"Состояние: Form.name. Сообщение: {message.text}")
+    if not message.text.strip():  # Проверяем, что текст не пустой
+        await message.answer('Пожалуйста, введи свое имя.')
+        return
+    await state.update_data(name=message.text.strip())  # Сохраняем имя
+    await message.answer('Сколько тебе лет?')
+    await state.set_state(Form.age)
+    logging.debug(f"Имя сохранено: {message.text.strip()}, состояние: Form.age")
 
+# Хэндлер для ввода возраста
+@dp.message(Form.age)
+async def get_age(message: Message, state: FSMContext):
+    logging.debug(f"Состояние: Form.age. Сообщение: {message.text}")
+    if not message.text.isdigit():
+        await message.answer('Пожалуйста, введи число для возраста.')
+        return
+    await state.update_data(age=int(message.text))
+    await message.answer('В каком ты классе?')
+    await state.set_state(Form.grade)
+    logging.debug(f"Возраст сохранен: {message.text}, состояние: Form.grade")
 
+# Хэндлер для ввода класса
+@dp.message(Form.grade)
+async def get_grade(message: Message, state: FSMContext):
+    logging.debug(f"Состояние: Form.grade. Сообщение: {message.text}")
+    if not message.text.strip():  # Проверяем, что текст не пустой
+        await message.answer('Пожалуйста, введи свой класс.')
+        return
+    await state.update_data(grade=message.text.strip())
+    student_data = await state.get_data()
+    logging.debug(f"Данные ученика: {student_data}")
+
+    # Сохраняем данные в базу данных
+    conn = sqlite3.connect('school_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+    INSERT INTO students (name, age, grade) VALUES (?, ?, ?)
+    ''', (student_data['name'], student_data['age'], student_data['grade']))
+    conn.commit()
+    conn.close()
+
+    await message.answer(f"Отлично, {student_data['name']}! Твои данные внесены в базу школы.")
+    await state.clear()
+    logging.debug("Состояние очищено")
+
+# Запуск бота
 async def main():
-    # Запускаем polling
+    logging.debug("Запуск бота...")
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
